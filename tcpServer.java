91fb91d4d0da80e663ab4ad1f925fee8767bc696
Mathias Neerup <mane@mmmi.sdu.dk>
2018-10-08 09:53:09 +0200

    Don't stop

--- tcpServer.java
+++ tcpServer.java
@@ -1,13 +1,13 @@
 public class tcpServer {
     public static void main(String[] args) {
          MultiThreadedServer server = new MultiThreadedServer(9000);
-         new Thread(server).start();
-         
-         try {
-             Thread.sleep(20 * 1000);
-         } catch (InterruptedException e) {
-             e.printStackTrace();
-         }
+         Thread t1 = new Thread(server);
+	 t1.start();
+	 try {
+           t1.join(); 
+	 } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
          System.out.println("Stopping Server");
          server.stop();
 
